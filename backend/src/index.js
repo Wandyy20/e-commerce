0dfd4ee1b44ddfbc89ac556c0fe207c7be309f1d
Wandy Reynand Lim <wandylims20@gmail.com>
2026-07-02 17:40:25 +0700

    Initial express setup and auth routes

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const authRoutes = require('./routes/authRoutes')
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use('/api/auth', authRoutes)
+
+app.get("/", (req, res) => {
+    res.json({message: 'Server is running'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+})

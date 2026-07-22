de9747b3744f71e4f06ba1a844f62f04a02a101c
Wandy Reynand Lim <wandylims20@gmail.com>
2026-07-22 15:05:35 +0700

    feat: Added address routes for api

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use('/api/products', productRoutes)
 app.use('/api/cart', cartRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/categories', categoryRoutes)
-app.use('/api/address', require())
+app.use('/api/addresses', addressRoutes)
 
 app.get("/", (req, res) => {
     res.json({message: 'Server is running'})
